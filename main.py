0db279af9591127a4e075eb5598433fb262137e2
unravel <90047484+lkkings@users.noreply.github.com>
2023-10-14 01:49:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import asyncio
+import json
 
 import websockets
 
 from barrage import BarrageBuilder
 
 live_uri = ""
+live_user_name = ""
 ws_uri = "ws://20.55.80.86:8080/live"
 
 
 async def connect_to_websocket_server():
     async with websockets.connect(ws_uri) as websocket:
         async def callback(message: str):
-            websocket.send(message)
+            if json.loads(message)["nickname"] != live_user_name:
+                await websocket.send(message)
 
         barrage = BarrageBuilder().douyin() \
             .page(live_uri) \
@@ -27,4 +30,5 @@ async def connect_to_websocket_server():
 
 if __name__ == '__main__':
     live_uri = input("请输入你的直播间地址：")
+    live_user_name = input("请输入你的昵称：")
     asyncio.get_event_loop().run_until_complete(connect_to_websocket_server())
