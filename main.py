2c41e4914570dabb84564ab45910ee36a32a7a46
lkkings <2893891716@qq.com>
2023-10-13 14:22:57 +0800

    完善了点补充说明

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
+import asyncio
+
+import websockets
+
 from barrage import BarrageBuilder
 
+live_uri = ""
+ws_uri = ""
 
-async def callback(message: str):
-    print(message)
+
+async def connect_to_websocket_server():
+    async with websockets.connect(ws_uri) as websocket:
+        async def callback(message: str):
+            websocket.send(message)
+
+        barrage = BarrageBuilder().douyin() \
+            .page(live_uri) \
+            .port(8080) \
+            .on(callback) \
+            .build()
+        barrage.daemon = True
+        barrage.start()
+        while True:
+            reply = await websocket.recv()
+            barrage.send(reply)
 
 
 if __name__ == '__main__':
-    url = "https://live.douyin.com/450072839585"
-    barrage = BarrageBuilder().douyin()\
-        .page(url)\
-        .port(8080)\
-        .on(callback)\
-        .build()
-
-    barrage.daemon = True
-    barrage.start()
-    barrage.join()
+    asyncio.get_event_loop().run_until_complete(connect_to_websocket_server())
